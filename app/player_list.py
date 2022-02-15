761b565b59497bfbd9e7420a08445fa73ad39811
galinasamulenko <samalina17@gmail.com>
2022-02-15 16:35:36 +0800

    prepare Double-Linked List Implementation

---
+++ app/player_list.py
@@ -0,0 +1,11 @@
+# Create the class that will implement the double linked list
+#
+# Filename: player_list.py
+# Project: SRUS-HS-Game
+# Author: Halina Samulenka <20031748@tafe.wa.edu.au>
+# Date Created: 15/2/22
+# ----------------------------------------------------------------------
+
+class PlayerList:
+    def __init__(self):
+        self.head_node = None
