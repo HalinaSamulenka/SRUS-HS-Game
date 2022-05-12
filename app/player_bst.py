fd8966fd9f9e786a3a55bf924dc53cca0cb77a8b
galinasamulenko <samalina17@gmail.com>
2022-05-12 15:03:46 +0800

    Optimise the BST by creating a Balanced BST

--- app/player_bst.py
+++ app/player_bst.py
@@ -5,9 +5,12 @@
 # Author: Halina Samulenka <20031748@tafe.wa.edu.au>
 # Date Created: 21/4/22
 # ---------------------------------------------------------------------
-# from re import search
+
 from app import player
 from app.player_bnode import PlayerBNode
+
+import sys
+import math
 
 
 class PlayerBST:
@@ -123,5 +126,50 @@ class PlayerBST:
         elif name > current_node.player.name and \
                 current_node.right_node != None:
             return self._search(name, current_node.right_node)
+
+    def create_sorted_list(self, root, nodes):
+        """
+        This function traverse the skewed binary tree and stores its
+        nodes pointers in vector nodes[]
+        :param root:
+        :param nodes:
+        """
+        if self.root is None:
+            return
+        # Store nodes in Inorder (which is sorted order for BST)
+        self.create_sorted_list(root.left_node, nodes)
+        nodes.append(root)
+        self.create_sorted_list(root.right_node, nodes)
+
+    def recursive_construct_bst(self, nodes, start, end):
+        """
+        Recursive function to construct binary tree
+        :param nodes:
+        :param start:
+        :param end:
+        :return: node
+        """
+        if start > end:
+            return None
+        # Get the middle element and make it root
+        mid = (start + end)//2
+        node = nodes[mid]
+        # construct left and right subtrees
+        node.left_node = self.recursive_construct_bst(nodes, start,mid-1)
+        node.right_node = self.recursive_construct_bst(nodes,
+                                                       mid + 1, end)
+        return node
+
+    def convert_bst_to_balanced_bst(self, root):
+        """
+        # This functions converts an unbalanced BST to a balanced BST
+        :param root:
+        """
+        # Store nodes of given BST in sorted order
+        nodes = []
+        self.create_sorted_list(root, nodes)
+        # Constructs BST from nodes[]
+        n = len(nodes)
+        return self.recursive_construct_bst(nodes, 0, n-1)
 
 
